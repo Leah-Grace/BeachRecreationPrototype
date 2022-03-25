46658cdccce87e4e70b1a9f7a28e508a4e5213b6
Leah Grace <Leahgdg@hotmail.com>
2022-03-25 15:58:07 -0400

    first commit

---
+++ src/com/LeahGrace/GetReport.java
@@ -0,0 +1,17 @@
+package com.LeahGrace;
+
+static class Report {
+    //  Conditions:
+    //  LOGISTICS:
+        //  Date
+        //  Time of Day
+        //  Sunrise and Sunset
+        //  Tide Chart
+    // Weather
+        //  Wind Speed (sustained or gusting)
+        //  Wind direction
+        //  Air temperature
+        //  Water temperature
+    // Waves
+
+}
